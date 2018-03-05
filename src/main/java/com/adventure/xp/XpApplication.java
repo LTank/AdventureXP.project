89e614de88e38aca46e6579ec02a69d5fb1aa835
Gustav Bang <gustavbang96@gmail.com>
2018-03-05 11:47:52 +0100

    Initial Commit

---
+++ src/main/java/com/adventure/xp/XpApplication.java
@@ -0,0 +1,12 @@
+package com.adventure.xp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XpApplication.class, args);
+	}
+}
