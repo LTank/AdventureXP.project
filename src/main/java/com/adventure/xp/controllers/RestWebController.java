a11f85c3f1188bc753cc905c5a80785895f9be7b
Simon_lfr <simonryom@gmail.com>
2018-03-11 19:23:49 +0100

    added some testing

---
+++ src/main/java/com/adventure/xp/controllers/RestWebController.java
@@ -0,0 +1,43 @@
+package com.adventure.xp.controllers;
+
+
+import com.adventure.xp.models.Event;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/event")
+public class RestWebController {
+
+    @GetMapping(value = "/all")
+    public String getEvents() {
+        String jsonMsg = null;
+        try {
+            List<Event> events = new ArrayList<Event>();
+            Event event = new Event();
+            event.setTitle("first event");
+            event.setStart("2018-03-01");
+            event.setEnd("2018-03-01");
+            events.add(event);
+
+            event = new Event();
+            event.setTitle("second event");
+            event.setStart("2018-03-11");
+            event.setEnd("2018-03-11");
+            events.add(event);
+
+            ObjectMapper mapper = new ObjectMapper();
+            jsonMsg = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(events);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return jsonMsg;
+    }
+
+}
