12037516e628ecd80a19bd942a0f7d8e31f01907
Unknown <theisko@gmail.com>
2018-03-12 12:37:23 +0100

    added heightlimit, description and maxattendants to Activity class

--- src/main/java/com/adventure/xp/models/Activity.java
+++ src/main/java/com/adventure/xp/models/Activity.java
@@ -4,17 +4,24 @@ public class Activity {
     private int id;
     private String name;
     private int ageLimit;
+    private int heightLimit;
+    private int maxAttendants;
     private double price;
+    private String calendarColor;
+    private String description;
 
-    public Activity(int id, String name, int ageLimit, double price) {
+    public Activity(int id, String name, int ageLimit, int heightLimit, int maxAttendants, double price, String calendarColor, String description) {
         this.id = id;
         this.name = name;
         this.ageLimit = ageLimit;
+        this.heightLimit = heightLimit;
+        this.maxAttendants = maxAttendants;
         this.price = price;
+        this.calendarColor = calendarColor;
+        this.description = description;
     }
 
     public Activity() {
-
     }
 
     public int getId() {
@@ -48,14 +55,51 @@ public class Activity {
     public void setPrice(double price) {
         this.price = price;
     }
+    public int getHeightLimit() {
+        return heightLimit;
+    }
+
+    public void setHeightLimit(int heightLimit) {
+        this.heightLimit = heightLimit;
+    }
+
+    public int getMaxAttendants() {
+        return maxAttendants;
+    }
+
+    public void setMaxAttendants(int maxAttendants) {
+        this.maxAttendants = maxAttendants;
+    }
+
+    public String getCalendarColor() {
+        return calendarColor;
+    }
+
+    public void setCalendarColor(String calendarColor) {
+        this.calendarColor = calendarColor;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
     @Override
     public String toString() {
         return "Activity{" +
                 "id=" + id +
-                ", name='" + name + '\'' +
+                ", name='" + name +
                 ", ageLimit=" + ageLimit +
                 ", price=" + price +
+                ", heightLimit=" + heightLimit +
+                ", maxAttendants=" + maxAttendants +
+                ", calendarColor=" + calendarColor +
+                ", description=" + description +
                 '}';
     }
+
+
 }
