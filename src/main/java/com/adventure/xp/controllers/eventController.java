de7d65a5fbef47f4daf27f822c5075e99f560061
Simon_lfr <simonryom@gmail.com>
2018-03-13 14:48:25 +0100

    Not functioning event url
    WIP

--- src/main/java/com/adventure/xp/controllers/eventController.java
+++ src/main/java/com/adventure/xp/controllers/eventController.java
@@ -1,13 +1,19 @@
 package com.adventure.xp.controllers;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.awt.*;
 
 @Controller
 public class eventController {
 
-    @GetMapping (value = "/event")
-    public String pointUrl () {
+    @RequestMapping(value = "/event", method = RequestMethod.GET)
+    public String pointUrl (@RequestParam("eventId")String eventId) {
+        System.out.println(eventId);
         return "index";
     }
 }
