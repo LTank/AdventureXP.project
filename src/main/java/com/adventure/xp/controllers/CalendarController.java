2d6a31ef747681667de0530e1bf5f357e89f73d3
Simon_lfr <simonryom@gmail.com>
2018-03-15 10:44:28 +0100

    redundant code

--- src/main/java/com/adventure/xp/controllers/CalendarController.java
+++ src/main/java/com/adventure/xp/controllers/CalendarController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
-public class calendarController {
+public class CalendarController {
 
 //    @GetMapping("/calendar")
 //    public String calendar() {
