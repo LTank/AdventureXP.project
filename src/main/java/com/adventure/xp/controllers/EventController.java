56ec22cb99c02c6cd60b9ce0ba804440e5c5fd46
Simon_lfr <simonryom@gmail.com>
2018-03-15 11:12:56 +0100

    commented out redundant code

--- src/main/java/com/adventure/xp/controllers/EventController.java
+++ src/main/java/com/adventure/xp/controllers/EventController.java
@@ -4,14 +4,13 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-
-
 @Controller
 public class EventController {
 
-    @RequestMapping(value = "/event", method = { RequestMethod.GET, RequestMethod.POST }, produces = MediaType.APPLICATION_PROBLEM_JSON_VALUE)
+    /*@RequestMapping(value = "/event", method = { RequestMethod.GET, RequestMethod.POST }, produces = MediaType.APPLICATION_PROBLEM_JSON_VALUE)
     public @ResponseBody String eventUrl (@RequestParam(value ="eventId")String id) {
         System.out.println(id);
         return "index";
     }
+    */
 }
