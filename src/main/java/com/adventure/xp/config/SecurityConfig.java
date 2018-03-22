eade357dcdc455ccaf6932f7a61ce76582e0918e
Unknown <chuwyn@live.dk>
2018-03-22 10:00:46 +0100

    Remove all crap added when trying to fix

--- src/main/java/com/adventure/xp/config/SecurityConfig.java
+++ src/main/java/com/adventure/xp/config/SecurityConfig.java
@@ -6,22 +6,18 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-@EnableWebSecurity(debug = true)
+@EnableAutoConfiguration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Qualifier("dataSource")
     @Autowired
@@ -34,7 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.jdbcAuthentication().dataSource(dataSource)
                 .authoritiesByUsernameQuery("select u.username, ur.role from users u inner join user_roles ur on(u.username=ur.username) where u.username=?")
                 .usersByUsernameQuery("select username, password, enabled from users where username=?")
-                .passwordEncoder(bCryptPasswordEncoder);
+                .passwordEncoder(passwordEncoder());
     }
 
     // This method can give you access or limit your access to pages. If you do not have access it will redirect to 403
@@ -51,9 +47,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     // Password encryption. I used https://www.dailycred.com/article/bcrypt-calculator to ocnvert password "1234".
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder(){
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    public PasswordEncoder passwordEncoder(){
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
         return encoder;
     }
 
